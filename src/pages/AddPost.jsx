806d215b9e648e04be86ef52da47678a3229d359
Philgyam <philemongyamfi9@gmail.com>
2024-04-23 20:01:27 +0000

    update

--- src/pages/AddPost.jsx
+++ src/pages/AddPost.jsx
@@ -3,10 +3,15 @@ import { useForm } from 'react-hook-form'
 import TextEditor from '../components/TextEditor'
 import Sidebar from '../components/Sidebar'
 import { ThemeContext } from '../components/ThemeProvider'
+import {UsernameContext} from '../components/UsernameContext'
+
+
 
 
 
 function AddPost() {
+
+  const {username,updateAvatar,avatar,setAvatar,updateUsername} = useContext(UsernameContext)
 
   const {theme,updateTheme} = useContext(ThemeContext)
 
@@ -15,10 +20,16 @@ function AddPost() {
   const onSubmit =(data)=>{
     console.log(data)
   }
+  
   return (
     <div div className={`flex flex-col h-screen pt-3   w-full ${theme.backgroundColor} `}>
-        <Sidebar/>
-      <div className='mb-10 text-2xl text-center mt-5'>
+      <div className='flex '>
+   <Sidebar/> 
+   <img className='rounded-full w-16 h-16 mr-2' src={avatar} alt=""  />
+
+      </div>
+     
+      <div className='mb-5 text-gray-400 text-2xl text-center mt-5'>
         Add Post
       </div>
       <form onSubmit={handleSubmit(onSubmit)}>
