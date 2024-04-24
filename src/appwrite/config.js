a4cfe59b56ad19aa0d7bb470bee2909e3e7c9250
Philgyam <philemongyamfi9@gmail.com>
2024-04-24 19:59:43 +0000

    update

--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -1,10 +1,29 @@
-import { Client, Databases } from "appwrite";
+import conf from "../conf/conf";
 
-const client = new Client();
+import { Client, Databases,Storage,Query } from "appwrite";
 
-const databases = new Databases(client);
 
-client
-    .setEndpoint('https://cloud.appwrite.io/v1') // Your API Endpoint
-    .setProject('66201769ed5710073074') // Your project ID
-;
+export class Service {
+    client = new Client()
+    databases;
+    bucket;
+
+    constructor(){
+        this.client.setEndpoint(conf.appwriteUrl)
+        .setProject(conf.appwriteProjectId)
+       
+
+        this.bucket = new Storage(this.client);
+
+        this.databases = new Databases(this.client);
+    }
+
+    async getPost(slug){
+    try {
+        
+    } catch (error) {
+        
+    }
+    }
+}
+
