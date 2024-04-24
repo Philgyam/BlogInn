703719de9aa252ee96a9bb0b489f818ff7c32827
Philgyam <philemongyamfi9@gmail.com>
2024-04-24 21:08:17 +0000

    "Add error handling to getPost() and add getPosts() method in Service class"
    This commit adds error handling to the getPost() method in the Service class, so that if an error occurs while fetching a post, it

--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -19,11 +19,25 @@ export class Service {
     }
 
     async getPost(slug){
-    try {
+    
+        try {
         
+            return await this.databases.getDocument(conf.appwriteProjectId,conf.appwriteCollectionId,slug)
     } catch (error) {
-        
+        console.log('get Post()',error)
+        return false
     }
+    }
+
+    async getPosts(queries = [queries = [Query.equal('category','sport')]]){
+
+        try {
+          return await  this.databases.listDocuments(conf.appwriteDatabaseId,conf.appwriteCollectionId,queries)
+        } catch (error) {
+            console.log(error,'get Post')
+            return false
+        }
+
     }
 }
 
