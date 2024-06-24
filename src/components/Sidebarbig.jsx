222c4cdfef2e0062e6fccf68b18f44d42bc43ff7
Philgyam <philemongyamfi9@gmail.com>
2024-06-24 13:26:23 +0000

    commit

--- src/components/Sidebarbig.jsx
+++ src/components/Sidebarbig.jsx
@@ -72,12 +72,12 @@ function Sidebarbig() {
         <img className='rounded-full w-6 h-6 md:w-5 md:h-5' src={avatar} alt="" />
       </div>
       <h2 className="text-xl md:text-2xl font-bold p-2 md:p-4"></h2>
-      <ul className="flex flex-col space-y-3 md:space-y-4 p-2 md:p-4">
+      <ul className="flex flex-col space-y-1 md:space-y-2 p-2 md:p-4 text-left">
         <li className="hover:bg-gray-700 p-1 md:p-2 rounded">
           <Link to='/Home'>
             <button
-              className=' py-1 px-2 md:py-2 md:px-4 rounded-full  lg:text-[1rem] md:text-[.5rem]'
-              >
+              className=' py-1 px-2 md:py-2 md:px-4 rounded-full lg:text-[1rem] md:text-[.75rem] text-left w-full'
+            >
               Home
             </button>
           </Link>
@@ -85,8 +85,8 @@ function Sidebarbig() {
         <li className="hover:bg-gray-700 p-1 md:p-2 rounded">
           <Link to='/Profile'>
             <button
-              className=' py-1 px-2 md:py-2 md:px-4 rounded-full  lg:text-[1rem] md:text-[.5rem]'
-              >
+              className=' py-1 px-2 md:py-2 md:px-4 rounded-full lg:text-[1rem] md:text-[.75rem] text-left w-full'
+            >
               My Page
             </button>
           </Link>
@@ -94,7 +94,7 @@ function Sidebarbig() {
         <li className="hover:bg-gray-700 p-1 md:p-2 rounded">
           <Link to='/addPost'>
             <button
-              className=' py-1 px-2 md:py-2 md:px-4 rounded-full  lg:text-[1rem] md:text-[.5rem]'
+              className=' py-1 px-2 md:py-2 md:px-4 rounded-full lg:text-[1rem] md:text-[.75rem] text-left w-full'
             >
               Add Post
             </button>
@@ -103,22 +103,20 @@ function Sidebarbig() {
         <li className="hover:bg-gray-700 p-1 md:p-2 rounded">
           <Link to='/'>
             <button
-              className=' py-1 px-2 md:py-2 md:px-4 rounded-full  lg:text-[1rem] md:text-[.5rem]'
+              className=' py-1 px-2 md:py-2 md:px-4 rounded-full lg:text-[1rem] md:text-[.75rem] text-left w-full'
               onClick={handleSubmit}
             >
               Logout
             </button>
           </Link>
         </li>
-        <button>
-
-        
         <li className="hover:bg-gray-700 p-1 md:p-2 rounded">
-          Community
-          </li>
+          <button className="w-full text-left">
+            Community
           </button>
+        </li>
       </ul>
-      <div className='flex flex-row justify-end gap-2 md:gap-4 pt-4 pr-4'>
+      <div className='flex flex-row justify-end gap-2 md:gap-3 pt-4 pr-4'>
         <div onClick={colorChange1} className='w-4 h-4 md:w-5 md:h-5 bg-[#FFF3C7] rounded-full hover:cursor-pointer'></div>
         <div onClick={colorChange2} className='w-4 h-4 md:w-5 md:h-5 bg-[#068DA9] rounded-full hover:cursor-pointer'></div>
         <div onClick={colorChange3} className='w-4 h-4 md:w-5 md:h-5 rounded-full bg-[#ECF8F9] hover:cursor-pointer'></div>
